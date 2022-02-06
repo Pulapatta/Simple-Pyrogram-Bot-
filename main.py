970c6c6058c00797130c03f6917875dbc533bca3
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2022-02-06 21:16:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ PHOTO_LINK = [
  "https://telegra.ph/file/4a842ee4ee51732835c57.jpg"
  ]
 
-Muhammed = Client(
+Muhammed= Client(
     "Pyrogram Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
